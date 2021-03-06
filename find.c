9f05369896b94af8cef7232bf117e44510acc490
m0rtal <vladi.github@gmail.com>
2021-03-06 22:27:36 +0200

    changed strcpy calls to snprintf

--- find.c
+++ find.c
@@ -27,11 +27,12 @@ void search(const char* start_dir, const char* target_file_name)
         // If curr dir entry is DT_DIR -> recursive search inside
         if (curr_dir_entry->d_type == DT_DIR)
         {
-            char* curr_dir_name = calloc(strlen(curr_dir_entry->d_name)+1, sizeof(char));
-            strcpy(curr_dir_name, curr_dir_entry->d_name);
+            size_t curr_dir_name_len = strlen(curr_dir_entry->d_name) + 1;
+            char* curr_dir_name = calloc(curr_dir_name_len, sizeof(char));
+            snprintf(curr_dir_name, curr_dir_name_len, "%s", curr_dir_entry->d_name);
             if (strcmp(curr_dir_name, ".") != 0 && strcmp(curr_dir_name, "..") != 0)
             {
-                int total_len = strlen(start_dir) + strlen(curr_dir_name) + 2;
+                size_t total_len = strlen(start_dir) + strlen(curr_dir_name) + 2;
                 char* next_start_dir = calloc(total_len,sizeof(char));
                 snprintf(next_start_dir, total_len, "%s/%s", start_dir, curr_dir_name);
 
